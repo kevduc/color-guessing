a1576929885597ccda364969cac9d648810b5851
kevduc <kduconge@gmail.com>
2022-10-07 22:14:06 +0100

    Fixed Next button not selectable with keyboard

--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -1,10 +1,16 @@
 import { FunctionComponent } from 'react'
 import styles from './NextButton.module.scss'
 
-interface NextButtonProps {}
+interface NextButtonProps {
+  [key: string]: any
+}
 
-const NextButton: FunctionComponent<NextButtonProps> = () => {
-  return <a className={`${styles['next-button']}`}>Next</a>
+const NextButton: FunctionComponent<NextButtonProps> = ({ ...rest }) => {
+  return (
+    <button className={`${styles['next-button']}`} {...rest}>
+      Next
+    </button>
+  )
 }
 
 export default NextButton
